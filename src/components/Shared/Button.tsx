9727cfb59edcc03ba18d89a443313a4b38e9cc70
Ioannis Kokkinidis <sudoplz@gmail.com>
2024-06-08 16:42:58 +0300

    feat: the button now supports both opacity and highlight modes

--- src/components/Shared/Button.tsx
+++ src/components/Shared/Button.tsx
@@ -1,28 +1,36 @@
-import * as React from 'react';
-import { Pressable, Text, StyleSheet, ViewStyle } from 'react-native';
-
-import { useTheme } from '../../theme/useTheme';
+import React from 'react';
+import { Text, StyleSheet, ViewStyle } from 'react-native';
+import {
+  TouchableOpacity,
+  TouchableHighlight,
+} from 'react-native-gesture-handler';
+// import { useTheme } from '../../theme/useTheme';
 
 export type ButtonProps = {
   onPress: () => void;
   text?: string;
   children?: React.ReactNode;
+  mode?: 'opacity' | 'highlight';
   style?: ViewStyle;
 };
+type TouchableType = typeof TouchableOpacity | typeof TouchableHighlight;
 
-const Button = ({ onPress, text, children, style }: ButtonProps) => {
-  const { theme } = useTheme();
+const Button = ({ onPress, text, children, style, mode }: ButtonProps) => {
+  // const { theme } = useTheme();
+  let Touchable: TouchableType = TouchableOpacity;
+  if (mode === 'highlight') {
+    Touchable = TouchableHighlight;
+  }
   return (
-    <Pressable
-      style={({ pressed }) => [
-        styles.container,
-        { backgroundColor: pressed ? `${theme.primary}ee` : theme.primary },
-        style,
-      ]}
+    <Touchable
+      style={[styles.container, style]}
+      containerStyle={styles.touchableContainer}
       onPress={onPress}>
-      {children ? children : <></>}
-      {text ? <Text style={styles.text}>{text}</Text> : <></>}
-    </Pressable>
+      <>
+        {children ? children : <></>}
+        {text ? <Text style={styles.text}>{text}</Text> : <></>}
+      </>
+    </Touchable>
   );
 };
 
@@ -30,9 +38,9 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 16,
     paddingVertical: 8,
-    backgroundColor: 'purple',
     borderRadius: 8,
   },
+  touchableContainer: {},
   text: { color: 'white' },
 });
 
