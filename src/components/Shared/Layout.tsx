13d657d7c27c49a2316b61629b6b3b413249e9d4
Ioannis Kokkinidis <sudoplz@gmail.com>
2024-06-13 01:59:45 +0300

    feat: changing status bar color for android

--- src/components/Shared/Layout.tsx
+++ src/components/Shared/Layout.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  StatusBar,
+  Platform,
+} from 'react-native';
 import { useTheme } from '../../theme/useTheme';
 import EightSleepLogo from '../Shared/EightSleepLogo';
 import { LayoutPropsType } from '../../types/components';
 import { ThemeContextInterface } from '../../theme/useTheme';
 
 const LOGO_WIDTH = Dimensions.get('window').width * 2.9;
+const androidStatusBarController =
+  Platform.OS === 'ios' ? null : <StatusBar backgroundColor="black" />;
 const Layout = ({ children, style, showLogo }: LayoutPropsType) => {
   const { theme }: Partial<ThemeContextInterface> = useTheme();
   return (
     <View
       testID="Layout.LayoutContainer"
       style={[styles.layout, { backgroundColor: theme?.layoutBg }, style]}>
+      {androidStatusBarController}
       {showLogo ? (
         <View style={styles.logoContainer}>
           <EightSleepLogo color={theme.cardBg} width={LOGO_WIDTH} />
